04dd3602257e1bcc553cca6eaf64a559785eeb19
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-02-18 19:20:02 -0600

    Se  agrego la graffica de codo

--- Hoja de trabajo clustering.R
+++ Hoja de trabajo clustering.R
@@ -4,7 +4,16 @@ library(mclust)
 library(fpc) 
 library(NbClust) 
 library(factoextra) 
-datos <- read.csv("C:/Users/LENOVO/Desktop/Clases/Minería de datos/HT2/HT2.Clustering/data/tmdb-movies.csv")
 setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/HT2/HT2.Clustering")
+datos <- read.csv("C:/Users/LENOVO/Desktop/Clases/Minería de datos/HT2/HT2.Clustering/data/tmdb-movies.csv")
 
+
+#Para saber cual es el mejor numero de cluste
+Variables_analizar = c("popularity", "budget", "revenue", "runtime" , "vote_count","vote_average")
+wss <- (nrow(datos[,Variables_analizar])-1)*sum(apply(datos[,Variables_analizar],2,var))
+
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(datos[,c("popularity", "budget" , "revenue" , "runtime" , "vote_count","vote_average")], centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
 
