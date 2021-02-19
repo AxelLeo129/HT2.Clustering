74ba47dc4e5439a5d3b35446237cb501244e352f
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-02-18 18:45:19 -0600

    Hoja de trabajo clustering

---
+++ Hoja de trabajo clustering.R
@@ -0,0 +1,10 @@
+library(cluster) 
+library(e1071)
+library(mclust) 
+library(fpc) 
+library(NbClust) 
+library(factoextra) 
+datos <- read.csv("C:/Users/LENOVO/Desktop/Clases/Minería de datos/HT2/HT2.Clustering/data/tmdb-movies.csv")
+setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/HT2/HT2.Clustering")
+
+
